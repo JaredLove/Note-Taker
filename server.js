da0bfb2113e2a4986affab9550e84d9dd9e35f78
Jared Love <lovejared91@yahoo.com>
2022-09-28 21:35:10 -0500

    added api routes

--- server.js
+++ server.js
@@ -8,3 +8,27 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+
+
+
+
+
+
+
+app.get('/api/notes', (req, res) => {
+  let results = (notes);
+
+ 
+  res.json(results);
+ 
+  
+})
+
+app.get('/api/notes/:id', (req, res) => {
+const result = findById(req.params.id, notes);
+  if (result) {
+  res.json(result);
+} else {
+  res.send(404);
+}
+});
