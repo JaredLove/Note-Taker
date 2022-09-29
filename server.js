20a0b97337ea9cd2dc66d77b4de5e3160f5d523f
Jared Love <lovejared91@yahoo.com>
2022-09-28 21:35:46 -0500

    added app.post and app.listen

--- server.js
+++ server.js
@@ -32,3 +32,34 @@ const result = findById(req.params.id, notes);
   res.send(404);
 }
 });
+
+
+app.post('/api/notes', (req, res) => {
+
+  req.body.id = notes.length.toString();
+  if ((!req.body)) {
+      res.status(400).send('The note is not properly formatted.');
+    } else {
+      const note = createNewNote(req.body, notes);
+      res.json(note);
+    }
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.listen(PORT, () => {
+
+  console.log(`API server now on port ${PORT}!`);
+
+});
